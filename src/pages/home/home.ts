8bc35088a89fbe1033d45adbe1594ce2f98556c7
KVDinesh <venkatdineshreddy.kothapalli@siu.edu>
2017-11-30 15:01:11 -0600

    scanning and displaying data using barcode scanner library

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  result : string;
+  format : string;
+  constructor(public navCtrl: NavController,private scan: BarcodeScanner) {
 
   }
 
+  ionViewWillEnter()
+  {
+    this.result = '';
+    this.format = '';
+  }
+openScanner()
+  {
+    this.scan.scan().then((data) => {
+      // Success! Barcode data is here
+        this.result = data.text;
+        this.format =  data.format
+     }, (err) => {
+         // An error occurred
+         alert(err)
+     });
+  }
 }
